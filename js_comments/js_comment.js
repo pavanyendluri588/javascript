49398748d90198393d3975af8a1e4cbb3d81809b
pavanyendluri588 <pavanyendluri588@gmail.com>
2022-09-02 21:04:30 +0530

    april_2nd_upload

---
+++ js_comments/js_comment.js
@@ -0,0 +1,49 @@
+//in js there are two types of cimments 
+//1.single line comments (//)
+//2.multiline comments (/*......................*/)
+
+
+//this is single line ccomment 
+
+/*
+ this 
+ is 
+ multiline
+ comment 
+ */
+
+
+
+
+ /*
+ js declaring a identifiers
+ 1.name can contain letters,digits,underscore,doller
+ 2.name must be begin with letter or unserscore(_) or dollersign ($)
+ 3.names are case sensitive (y and Y are different)
+ 4.js keywords cannot be used as identifier 
+
+ 
+ */
+
+ /*
+ A variable declared without a value will have the value undefined.
+
+
+ You cannot re-declare a variable declared with let or const.
+
+ {
+  let x = 2;
+}
+// x can NOT be used here
+
+
+let x = 10;
+// Here x is 10
+
+{
+let x = 2;
+// Here x is 2
+}
+
+// Here x is 10
+*/
---
+++ js_variables/js_var.js
@@ -0,0 +1,30 @@
+//variable declaring using the var 
+var a ;
+a=15
+console.log("before updation",a)
+//variable with var cannot  be declared without initilization
+//the varible is declared by using var can be redeclared  or updated 
+//It can be accessed without initialization as its default value is “undefined”.
+var a =20;
+console.log("after updation:",a)
+
+/
+//variable declaring using the let 
+
+let b = 10;
+console.log(b)
+//It cannot be accessed without initialization otherwise it will give ‘referenceError
+//variable can be declared without let keyword can be considered as declaring let 
+c=30
+console.log(c)
+//variable with const let  be declared without initilization
+//the variable declared by using let cannot be redeclard but the value can be updated
+c=50
+console.log("after updation of c:",c)
+
+
+//It cannot be accessed without initialization otherwise it will give ‘referenceError
+//variable is declared by using const 
+const d =500
+//variable with const cannot be redeclared  and cannot be upated
+//variable with const cannot  be declared without initilization
