5cd4b60994f4726deb5abd9a95f94278f6b52f09
pavanyendluri588 <pavanyendluri588@gmail.com>
2022-11-09 17:53:04 +0530

    april_2nd_upload

---
+++ strings/string.js
@@ -0,0 +1,42 @@
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+console.log(text.length);
+
+text = "We are the so-called \'Vikings\' from the north.";
+console.log(text);
+
+text = "We are the so-called \"Vikings\" from the north.";
+console.log(text);
+
+text = "We are the so-called \\Vikings\\ from the north.";
+console.log(text);
+
+
+/*
+\b
+\f
+\n
+\r
+\t
+\v
+
+*/
+
+//breaking long code lines 
+//using the back slash symbol (\)
+
+
+/* this  will give error because of the white space between the text 
+console.log("Hello 
+Dolly!");
+*/
+
+
+
+
+console.log("Hello \
+Dolly!");
+
+
+
+
+
